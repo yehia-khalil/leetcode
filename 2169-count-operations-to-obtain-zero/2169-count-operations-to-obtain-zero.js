99f62737d3cf19974d80c8811cce887e361883be
Yehia Khalil <76862901+yehia-khalil@users.noreply.github.com>
2022-05-27 14:37:10 +0200

    Time: 76 ms (62.93%), Space: 41.9 MB (68.29%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var countOperations = function(num1, num2) {
+    let counter = 0;
+  while(num1 > 0 && num2 > 0){
+    num1 < num2 ? num2 -= num1 : num1 -= num2;
+    counter ++;
+  }
+  return counter;
+};
