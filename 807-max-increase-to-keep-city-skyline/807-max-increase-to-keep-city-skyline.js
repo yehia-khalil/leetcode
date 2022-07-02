ee8344b801e8bc3ae7a570c6c20ed042ddf20add
Yehia Khalil <76862901+yehia-khalil@users.noreply.github.com>
2022-07-02 10:18:58 +0200

    Time: 82 ms (80.80%), Space: 44.9 MB (25.00%) - LeetHub

--- 807-max-increase-to-keep-city-skyline/807-max-increase-to-keep-city-skyline.js
+++ 807-max-increase-to-keep-city-skyline/807-max-increase-to-keep-city-skyline.js
@@ -4,19 +4,35 @@
  */
 var maxIncreaseKeepingSkyline = function(grid) {
   let count=0;
+  let colMemo=new Array(grid[0].length);
+  
     for(let i = 0 ; i < grid[0].length ; i++){
+      let rowMax=getMaxRow(grid,i);
+      
       for(let j = 0 ; j < grid.length ; j++){
-        let max = getMax(grid,i,j);
-        if(grid[i][j]<max){
-          count+=max-grid[i][j];
+        
+        if(colMemo[j]) {
+          let max = Math.min(rowMax,colMemo[j]);
+          if(grid[i][j]<max) {
+            count += max-grid[i][j];
+          }
+        } else { 
+          colMemo[j] = getMaxCol(grid,j);
+          let max = Math.min(rowMax,colMemo[j]);
+          if(grid[i][j]<max) {
+            count+=max-grid[i][j];
+          }
         }
+
       }
     }
   return count;
 };
 
-function getMax(grid,row,col){
-  let rowMax = Math.max(...grid[row]);
-  let colMax = Math.max(...grid.map((elem,index)=>elem[col]));
-  return Math.min(rowMax,colMax);
+function getMaxCol(grid,col){
+  return Math.max(...grid.map((elem,index)=>elem[col]));
+}
+
+function getMaxRow(grid,row){
+  return Math.max(...grid[row]);
 }
