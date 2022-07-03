0d279cc3ecf27998486427f3ab94a25df77e6239
Yehia Khalil <76862901+yehia-khalil@users.noreply.github.com>
2022-07-03 13:45:21 +0200

    Time: 162 ms (69.43%), Space: 44.4 MB (72.29%) - LeetHub

---
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} boxes
+ * @return {number[]}
+ */
+var minOperations = function(boxes) {
+  let ones =[];
+  boxes.split("").filter((elem, i, arr)=>{
+    if(elem==1){
+      ones.push(i);
+    }
+  });
+  let res = [];
+  for(let i = 0 ; i < boxes.length ; i++){
+      let ops=0;
+      for(let j = 0 ; j < ones.length ; j++){
+        ops+=Math.abs(i-ones[j]);
+    }
+    res.push(ops);
+  }
+  return res;
+};
