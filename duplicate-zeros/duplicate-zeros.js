8093d83b44fe7e24236f3d20712e4081c4bd2791
Yehia Khalil <76862901+yehia-khalil@users.noreply.github.com>
2022-10-29 20:47:03 +0200

    Time: 105 ms, Memory: 44.5 MB - LeetHub

--- duplicate-zeros/duplicate-zeros.js
+++ duplicate-zeros/duplicate-zeros.js
@@ -3,12 +3,14 @@
  * @return {void} Do not return anything, modify arr in-place instead.
  */
 var duplicateZeros = function(arr) {
-  let length = arr.length
+  let res = [];
   for(let i = 0 ; i < arr.length ; i++){
+    res.push(arr[i]);
     if(arr[i]===0){
-      arr.splice(i,0,0);
-      i++;
+      res.push(0);
     }
   }
-  arr.length = length;
+  for(let i = 0 ; i < arr.length ; i++){
+    arr[i]=res[i];
+  }
 };
