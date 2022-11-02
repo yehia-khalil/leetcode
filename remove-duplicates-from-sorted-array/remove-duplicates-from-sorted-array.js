ad6d8b36089d497b5ed474c6b3cbe46cb2483004
Yehia Khalil <76862901+yehia-khalil@users.noreply.github.com>
2022-11-02 18:41:28 +0200

    Time: 152 ms, Memory: 44.6 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,13 +3,20 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-  loop1:
-    for(let i = 1 ; i < nums.length ; i++){
-      if(nums[i]==nums[i-1]){
-        nums.splice(i,1);
-        i--;
-        continue loop1;
-      }
+  let ptr;
+  let worm;
+  let x = 0;
+  ptr=worm=nums[x];
+  for(let i = 1 ; i <= nums.length ; i++){
+    worm=nums[i];
+    if(ptr!==worm){
+      x++;
+      nums[x]=worm;
+      ptr=nums[x];
+    }else{
+      continue;
     }
+  }
+  nums.length = x;
   return nums.length;
 };
