63d146561b276ce51bf49e9502dc5c4fff463621
Yehia Khalil <76862901+yehia-khalil@users.noreply.github.com>
2023-10-05 19:57:02 +0300

    Time: 241 ms (8.23%), Space: 63.2 MB (5.59%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var mostFrequentEven = function(nums) {
+    let map = {};
+    for(item of nums){
+        if(item%2 != 0){
+            continue;
+        }
+        if(map[item]!=undefined){
+            map[item]+=1;
+        }else{
+            map[item]=1;
+        }
+    }
+    console.log(map)
+    if(Object.keys(map).length==0){
+        return -1;
+    }
+    let memo = {};
+    for(item in map){
+        if(!memo[map[item]]){
+            memo[map[item]]=[item]
+        }else{
+            memo[map[item]]=[...memo[map[item]],item]
+        }
+    }
+    let min=undefined
+    for(item in memo){
+        min=Math.min(...memo[item]);
+    }
+    return min??-1;
+};
